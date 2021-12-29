76d7f41b29c62cb223bc064495378dd9b25ba73e
gregoza <jgregoza@outlook.com>
2021-12-29 15:02:17 -0600

    Añadiendo el proyecto58

--- webpersonal/portfolio/views.py
+++ webpersonal/portfolio/views.py
@@ -3,5 +3,5 @@ from .models import Project
 
 # Create your views here.
 def portfolio(request):
-    projetcs = Project.objects.all()
-    return render(request, "portfolio/portfolio.html", {'projects':projetcs})
+    projects = Project.objects.all()
+    return render(request, "portfolio/portfolio.html", {'projects':projects})
